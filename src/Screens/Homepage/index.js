c4f6e99fd55098b76ba7f03ff5d266235370bdc4
Shubham Lakhmani <jordanrock@Shubhams-MacBook-Air.local>
2022-05-13 21:12:05 +0530

    Added antd carousel

--- src/Screens/Homepage/index.js
+++ src/Screens/Homepage/index.js
@@ -1,18 +1,38 @@
 import Navbar from "../../components/Navbar";
+import "antd/dist/antd.css";
+import { Carousel } from "antd";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
 
-      <div>
-        <img
-          src="/assets/images/perfume1.jpg"
-          width="100%"
-          height="auto"
-          alt="Hello"
-        />
-      </div>
+      <Carousel autoplay autoplaySpeed={3000}>
+        <div>
+          <img
+            src="/assets/images/img1.jpeg"
+            style={{ width: "100vw", height: "60vh" }}
+          />
+        </div>
+        <div>
+          <img
+            src="/assets/images/img2.jpeg"
+            style={{ width: "100vw", height: "60vh" }}
+          />
+        </div>
+        <div>
+          <img
+            src="/assets/images/img3.jpeg"
+            style={{ width: "100vw", height: "60vh" }}
+          />
+        </div>
+        <div>
+          <img
+            src="/assets/images/img4.jpeg"
+            style={{ width: "100vw", height: "60vh" }}
+          />
+        </div>
+      </Carousel>
     </div>
   );
 };
