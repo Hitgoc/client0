d0894f47c0fd7387d70a0494ac66ab72348ac7f3
Shubham Lakhmani <jordanrock@Shubhams-MacBook-Air.local>
2022-05-13 16:39:33 +0530

    Added HomePage

---
+++ src/Screens/Homepage/index.js
@@ -0,0 +1,20 @@
+import Navbar from "../../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <div>
+        <img
+          src="/assets/images/perfume1.jpg"
+          width="100%"
+          height="auto"
+          alt="Hello"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
